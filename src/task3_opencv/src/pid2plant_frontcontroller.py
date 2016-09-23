7e1e61cd9286650e2538a65797db803a1c7b3c89
Piergiuseppe <mallozzi@chalmers.se>
2016-09-23 17:34:16 +0200

    Started the PID2PLANT Front Controller

--- src/task3_opencv/src/pid2plant_frontcontroller.py
+++ src/task3_opencv/src/pid2plant_frontcontroller.py
@@ -1,134 +1,92 @@
 #!/usr/bin/env python
 
-# The Keyboard Controller Node for the tutorial "Up and flying with the AR.Drone and ROS | Getting Started"
-# https://github.com/mikehamer/ardrone_tutorials
+# A basic drone controller class for the tutorial "Up and flying with the AR.Drone and ROS | Getting Started"
+# https://github.com/mikehamer/ardrone_tutorials_getting_started
 
-# This controller extends the base DroneVideoDisplay class, adding a keypress handler to enable keyboard control of the drone
+# This class implements basic control functionality which we will be using in future tutorials.
+# It can command takeoff/landing/emergency as well as drone movement
+# It also tracks the drone state based on navdata feedback
 
 # Import the ROS libraries, and load the manifest file which through <depend package=... /> will give us access to the project dependencies
 import roslib; roslib.load_manifest('ardrone_tutorials')
 import rospy
 
-# Load the DroneController class, which handles interactions with the drone, and the DroneVideoDisplay class, which handles video display
-from drone_controller import BasicDroneController
-from drone_video_display import DroneVideoDisplay
+# Import the messages we're interested in sending and receiving
+from geometry_msgs.msg import Twist  	 # for sending commands to the drone
+from std_msgs.msg import Empty       	 # for land/takeoff/emergency
+from ardrone_autonomy.msg import Navdata # for receiving navdata feedback
 
-# Finally the GUI libraries
-from PySide import QtCore, QtGui
-
-
-# Here we define the keyboard map for our controller (note that python has no enums, so we use a class)
-class KeyMapping(object):
-	PitchForward     = QtCore.Qt.Key.Key_E
-	PitchBackward    = QtCore.Qt.Key.Key_D
-	RollLeft         = QtCore.Qt.Key.Key_S
-	RollRight        = QtCore.Qt.Key.Key_F
-	YawLeft          = QtCore.Qt.Key.Key_W
-	YawRight         = QtCore.Qt.Key.Key_R
-	IncreaseAltitude = QtCore.Qt.Key.Key_Q
-	DecreaseAltitude = QtCore.Qt.Key.Key_A
-	Takeoff          = QtCore.Qt.Key.Key_Y
-	Land             = QtCore.Qt.Key.Key_H
-	Emergency        = QtCore.Qt.Key.Key_Space
+# An enumeration of Drone Statuses
+from drone_status import DroneStatus
 
 
-# Our controller definition, note that we extend the DroneVideoDisplay class
-class KeyboardController(DroneVideoDisplay):
+# Some Constants
+COMMAND_PERIOD = 100 #ms
+
+
+class BasicDroneController(object):
 	def __init__(self):
-		super(KeyboardController,self).__init__()
-		
-		self.pitch = 0
-		self.roll = 0
-		self.yaw_velocity = 0 
-		self.z_velocity = 0
+		# Holds the current drone status
+		self.status = -1
 
-# We add a keyboard handler to the DroneVideoDisplay to react to keypresses
-	def keyPressEvent(self, event):
-		key = event.key()
+		# Subscribe to the /ardrone/navdata topic, of message type navdata, and call self.ReceiveNavdata when a message is received
+		self.subNavdata = rospy.Subscriber('control_effort',Navdata,self.ReceiveNavdata)
 
-		# If we have constructed the drone controller and the key is not generated from an auto-repeating key
-		if controller is not None and not event.isAutoRepeat():
-			# Handle the important cases first!
-			if key == KeyMapping.Emergency:
-				controller.SendEmergency()
-			elif key == KeyMapping.Takeoff:
-				controller.SendTakeoff()
-			elif key == KeyMapping.Land:
-				controller.SendLand()
-			else:
-				# Now we handle moving, notice that this section is the opposite (+=) of the keyrelease section
-				if key == KeyMapping.YawLeft:
-					self.yaw_velocity += 1
-				elif key == KeyMapping.YawRight:
-					self.yaw_velocity += -1
+		# Allow the controller to publish to the /ardrone/takeoff, land and reset topics
+		# self.pubLand    = rospy.Publisher('/ardrone/land',Empty,queue_size=1)
+		# self.pubTakeoff = rospy.Publisher('/ardrone/takeoff',Empty,queue_size=1)
+		# self.pubReset   = rospy.Publisher('/ardrone/reset',Empty,queue_size=1)
 
-				elif key == KeyMapping.PitchForward:
-					self.pitch += 1
-				elif key == KeyMapping.PitchBackward:
-					self.pitch += -1
+		# Allow the controller to publish to the /cmd_vel topic and thus control the drone
+		self.pubCommand = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
 
-				elif key == KeyMapping.RollLeft:
-					self.roll += 1
-				elif key == KeyMapping.RollRight:
-					self.roll += -1
+		# Setup regular publishing of control packets
+		self.command = Twist()
+		self.commandTimer = rospy.Timer(rospy.Duration(COMMAND_PERIOD/1000.0),self.SendCommand)
 
-				elif key == KeyMapping.IncreaseAltitude:
-					self.z_velocity += 1
-				elif key == KeyMapping.DecreaseAltitude:
-					self.z_velocity += -1
+		# Land the drone if we are shutting down
+		rospy.on_shutdown(self.SendLand)
 
-			# finally we set the command to be sent. The controller handles sending this at regular intervals
-			controller.SetCommand(self.roll, self.pitch, self.yaw_velocity, self.z_velocity)
+	def ReceiveNavdata(self,navdata):
+		# Although there is a lot of data in this packet, we're only interested in the state at the moment
+		self.status = navdata.state
 
+	def SendTakeoff(self):
+		# Send a takeoff message to the ardrone driver
+		# Note we only send a takeoff message if the drone is landed - an unexpected takeoff is not good!
+		if(self.status == DroneStatus.Landed):
+			self.pubTakeoff.publish(Empty())
 
-	def keyReleaseEvent(self,event):
-		key = event.key()
+	def SendLand(self):
+		# Send a landing message to the ardrone driver
+		# Note we send this in all states, landing can do no harm
+		self.pubLand.publish(Empty())
 
-		# If we have constructed the drone controller and the key is not generated from an auto-repeating key
-		if controller is not None and not event.isAutoRepeat():
-			# Note that we don't handle the release of emergency/takeoff/landing keys here, there is no need.
-			# Now we handle moving, notice that this section is the opposite (-=) of the keypress section
-			if key == KeyMapping.YawLeft:
-				self.yaw_velocity -= 1
-			elif key == KeyMapping.YawRight:
-				self.yaw_velocity -= -1
+	def SendEmergency(self):
+		# Send an emergency (or reset) message to the ardrone driver
+		self.pubReset.publish(Empty())
 
-			elif key == KeyMapping.PitchForward:
-				self.pitch -= 1
-			elif key == KeyMapping.PitchBackward:
-				self.pitch -= -1
+	def SetCommand(self,roll=0,pitch=0,yaw_velocity=0,z_velocity=0):
+		# Called by the main program to set the current command
+		self.command.linear.x  = pitch
+		self.command.linear.y  = roll
+		self.command.linear.z  = z_velocity
+		self.command.angular.z = yaw_velocity
 
-			elif key == KeyMapping.RollLeft:
-				self.roll -= 1
-			elif key == KeyMapping.RollRight:
-				self.roll -= -1
-
-			elif key == KeyMapping.IncreaseAltitude:
-				self.z_velocity -= 1
-			elif key == KeyMapping.DecreaseAltitude:
-				self.z_velocity -= -1
-
-			# finally we set the command to be sent. The controller handles sending this at regular intervals
-			controller.SetCommand(self.roll, self.pitch, self.yaw_velocity, self.z_velocity)
-
+	def SendCommand(self,event):
+		# The previously set command is then sent out periodically if the drone is flying
+		if self.status == DroneStatus.Flying or self.status == DroneStatus.GotoHover or self.status == DroneStatus.Hovering:
+			self.pubCommand.publish(self.command)
 
 
 # Setup the application
 if __name__=='__main__':
 	import sys
 	# Firstly we setup a ros node, so that we can communicate with the other packages
-	rospy.init_node('ardrone_keyboard_controller')
+	rospy.init_node('pid2plant_frontcontroller')
 
 	# Now we construct our Qt Application and associated controllers and windows
-	app = QtGui.QApplication(sys.argv)
 	controller = BasicDroneController()
-	display = KeyboardController()
-
-	display.show()
-
-	# executes the QT application
-	status = app.exec_()
 
 	# and only progresses to here once the application has been shutdown
-	rospy.signal_shutdown('Great Flying!')
-	sys.exit(status)
+	rospy.signal_shutdown('Great Control!')
