1e41cb21cf6247b42997c14c3d4420bbfbfbfba8
David Issa Mattos <issamattos.david@gmail.com>
2016-10-03 17:03:48 +0200

    PID 2 Plant + launch files

--- src/task3_opencv/src/pid2plant_frontcontroller.py
+++ src/task3_opencv/src/pid2plant_frontcontroller.py
@@ -1,134 +1,57 @@
 #!/usr/bin/env python
 
-# The Keyboard Controller Node for the tutorial "Up and flying with the AR.Drone and ROS | Getting Started"
-# https://github.com/mikehamer/ardrone_tutorials
+# A basic front controller to be used with the PID controller. Based on the drone controller class for the tutorial "Up and flying with the AR.Drone and ROS | Getting Started"
+# https://github.com/mikehamer/ardrone_tutorials_getting_started
 
-# This controller extends the base DroneVideoDisplay class, adding a keypress handler to enable keyboard control of the drone
+# This class implements the pid2plant part in the control loop
 
 # Import the ROS libraries, and load the manifest file which through <depend package=... /> will give us access to the project dependencies
-import roslib; roslib.load_manifest('ardrone_tutorials')
+import roslib
 import rospy
 
-# Load the DroneController class, which handles interactions with the drone, and the DroneVideoDisplay class, which handles video display
-from drone_controller import BasicDroneController
-from drone_video_display import DroneVideoDisplay
-
-# Finally the GUI libraries
-from PySide import QtCore, QtGui
+# Import the messages we're interested in sending and receiving
+from geometry_msgs.msg import Twist  	 # for sending commands to the drone
+from std_msgs.msg import Float64	 # for the control_effort
 
 
-# Here we define the keyboard map for our controller (note that python has no enums, so we use a class)
-class KeyMapping(object):
-	PitchForward     = QtCore.Qt.Key.Key_E
-	PitchBackward    = QtCore.Qt.Key.Key_D
-	RollLeft         = QtCore.Qt.Key.Key_S
-	RollRight        = QtCore.Qt.Key.Key_F
-	YawLeft          = QtCore.Qt.Key.Key_W
-	YawRight         = QtCore.Qt.Key.Key_R
-	IncreaseAltitude = QtCore.Qt.Key.Key_Q
-	DecreaseAltitude = QtCore.Qt.Key.Key_A
-	Takeoff          = QtCore.Qt.Key.Key_Y
-	Land             = QtCore.Qt.Key.Key_H
-	Emergency        = QtCore.Qt.Key.Key_Space
+# Some Constants
+COMMAND_PERIOD = 100 #ms
 
 
-# Our controller definition, note that we extend the DroneVideoDisplay class
-class KeyboardController(DroneVideoDisplay):
+class BasicPitchController(object):
 	def __init__(self):
-		super(KeyboardController,self).__init__()
 		
-		self.pitch = 0
-		self.roll = 0
-		self.yaw_velocity = 0 
-		self.z_velocity = 0
+		#subscribe to the PID control effort
+		self.controlEffort = rospy.Subscriber('control_effort/',Float64,self.ApplyControlEffort)		
+				
+		# Allow the controller to publish to the /cmd_vel topic and thus control the drone
+		self.pubCommand = rospy.Publisher('cmd_vel',Twist,queue_size=1)
 
-# We add a keyboard handler to the DroneVideoDisplay to react to keypresses
-	def keyPressEvent(self, event):
-		key = event.key()
+		# Setup regular publishing of control packets
+		self.command = Twist()
 
-		# If we have constructed the drone controller and the key is not generated from an auto-repeating key
-		if controller is not None and not event.isAutoRepeat():
-			# Handle the important cases first!
-			if key == KeyMapping.Emergency:
-				controller.SendEmergency()
-			elif key == KeyMapping.Takeoff:
-				controller.SendTakeoff()
-			elif key == KeyMapping.Land:
-				controller.SendLand()
-			else:
-				# Now we handle moving, notice that this section is the opposite (+=) of the keyrelease section
-				if key == KeyMapping.YawLeft:
-					self.yaw_velocity += 1
-				elif key == KeyMapping.YawRight:
-					self.yaw_velocity += -1
+	def SetCommand(self,roll=0,pitch=0,yaw_velocity=0,z_velocity=0):
+		# Called by the main program to set the current command
+		self.command.linear.x  = pitch
+		self.command.linear.y  = roll
+		self.command.linear.z  = z_velocity
+		self.command.angular.z = yaw_velocity
 
-				elif key == KeyMapping.PitchForward:
-					self.pitch += 1
-				elif key == KeyMapping.PitchBackward:
-					self.pitch += -1
-
-				elif key == KeyMapping.RollLeft:
-					self.roll += 1
-				elif key == KeyMapping.RollRight:
-					self.roll += -1
-
-				elif key == KeyMapping.IncreaseAltitude:
-					self.z_velocity += 1
-				elif key == KeyMapping.DecreaseAltitude:
-					self.z_velocity += -1
-
-			# finally we set the command to be sent. The controller handles sending this at regular intervals
-			controller.SetCommand(self.roll, self.pitch, self.yaw_velocity, self.z_velocity)
-
-
-	def keyReleaseEvent(self,event):
-		key = event.key()
-
-		# If we have constructed the drone controller and the key is not generated from an auto-repeating key
-		if controller is not None and not event.isAutoRepeat():
-			# Note that we don't handle the release of emergency/takeoff/landing keys here, there is no need.
-			# Now we handle moving, notice that this section is the opposite (-=) of the keypress section
-			if key == KeyMapping.YawLeft:
-				self.yaw_velocity -= 1
-			elif key == KeyMapping.YawRight:
-				self.yaw_velocity -= -1
-
-			elif key == KeyMapping.PitchForward:
-				self.pitch -= 1
-			elif key == KeyMapping.PitchBackward:
-				self.pitch -= -1
-
-			elif key == KeyMapping.RollLeft:
-				self.roll -= 1
-			elif key == KeyMapping.RollRight:
-				self.roll -= -1
-
-			elif key == KeyMapping.IncreaseAltitude:
-				self.z_velocity -= 1
-			elif key == KeyMapping.DecreaseAltitude:
-				self.z_velocity -= -1
-
-			# finally we set the command to be sent. The controller handles sending this at regular intervals
-			controller.SetCommand(self.roll, self.pitch, self.yaw_velocity, self.z_velocity)
-
-
+	
+	def ApplyControlEffort(self):
+		self.SetCommand(0,self.controlEffort,0,0)
+		self.pubCommand.publish(self.command)
 
 # Setup the application
 if __name__=='__main__':
 	import sys
 	# Firstly we setup a ros node, so that we can communicate with the other packages
-	rospy.init_node('ardrone_keyboard_controller')
+	rospy.init_node('pid2plant_frontcontroller')
 
-	# Now we construct our Qt Application and associated controllers and windows
-	app = QtGui.QApplication(sys.argv)
-	controller = BasicDroneController()
-	display = KeyboardController()
-
-	display.show()
-
-	# executes the QT application
-	status = app.exec_()
-
-	# and only progresses to here once the application has been shutdown
-	rospy.signal_shutdown('Great Flying!')
-	sys.exit(status)
+	#Temporary setpoint
+	pubCommand2 = rospy.Publisher('setpoint',Float64,queue_size=1)
+	pubCommand2.publish(1.0)
+	controller = BasicPitchController()
+	controller.ApplyControlEffort()	
+	#This keeps the function active till node are shurdown.
+	rospy.spin()
