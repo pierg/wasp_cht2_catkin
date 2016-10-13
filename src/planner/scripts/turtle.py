1ef32e4fd88779653c848ff5642e6a571f0a4fbc
Jonatan Kilhamn <jonkil@chalmers.se>
2016-10-13 16:32:08 +0200

    All set up for testing scheduler->turtle communication

--- src/planner/scripts/turtle.py
+++ src/planner/scripts/turtle.py
@@ -44,28 +44,57 @@ from planner.msg import drone_command # NB: turtle uses drone_command as well!
 
 
 
+def goto(x, y, w):
+	global actuatorTopic
+	#goto_to_publish.header.frame_id = "/map"
+	goto_to_publish.pose.position.x = x
+	goto_to_publish.pose.position.y = y
+	#goto_to_publish.pose.orientation.z = 1
+	goto_to_publish.pose.orientation.w = w
+
+	actuatorTopic.publish(goto_to_publish)
+
 
 def takeAction(data):
     global topic
     if (data.command != 'idle'):
         cdata = data._connection_header
-
         rospy.loginfo('%s now initiates %s', id, data.command)
-        rate.sleep()
-        #driveTo(data.posX, data.posY)
+        #TODO:
+        goto(data.posX, data.posY, data.angle)
 
 
         
-        msg = drone_command();
-        msg.drone_id = id;
-        msg.command = 'idle';
-        topic.publish(msg)
+
+
+
+#This callback is called everytime this node receives the status message of Robot
+#The Status message is published frequently from node, always has the last goal
+# status if no new goals are published.
+#Goal status information : http://docs.ros.org/api/actionlib_msgs/html/msg/GoalStatus.html
+#status = 1 -> Active
+#status = 4 -> failed
+#status = 3 -> completed
+def goal_status_callback(data):
+	global goal_flag
+	print(data.status_list[0].status)
+	if data.status_list[0].status == 1:
+            goal_flag = false
+	if data.status_list[0].status == 3:
+            if !goal_flag:
+                print "completed goal"
+                goal_flag = true
+	        msg = drone_command();
+                msg.drone_id = id;
+                msg.command = 'idle';
+                topic.publish(msg)
+
 
 
 def turtle(args):
     global rate
     global id
-    global topic
+    global schedulerTopic, actuatorTopic
     #global idRequestTopic
     #TODO: data validation of arg
 
@@ -73,8 +102,13 @@ def turtle(args):
     id = 'turtle'+arg
     rospy.init_node(id, anonymous=True)
     rospy.loginfo('started '+id)
-    topic = rospy.Publisher(id, drone_command, queue_size=10)
+    schedulerTopic = rospy.Publisher(id, drone_command, queue_size=10)
     rospy.Subscriber(id, drone_command, takeAction)
+    #Assign Publisher that publishes the goal to the robot to move
+    actuatorTopic = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+    #subscribe to goal status from mobile base
+    rospy.Subscriber("/move_base/status", GoalStatusArray, goal_status_callback)
+
     rate = rospy.Rate(1)
     rate.sleep() #has to sleep after subscribing to a new topic
 
@@ -83,8 +117,8 @@ def turtle(args):
     msg.command = 'idle';
     topic.publish(msg)
 
-    # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
