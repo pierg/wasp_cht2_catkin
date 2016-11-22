d7c6f0723b84c73cf225c0c97e6bb5518ffac32b
David Issa Mattos <issamattos.david@gmail.com>
2016-11-22 14:02:43 +0100

    correcting the transformation listener for the apriltags

--- src/drone_tf/src/apriltag_tf_broadcaster.cpp
+++ src/drone_tf/src/apriltag_tf_broadcaster.cpp
@@ -3,23 +3,56 @@
 
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
-#include "tb_self_experimentation/object_loc.h"
+#include "wasp_custom_msgs/object_loc.h"
+#include "std_msgs/Bool.h"
+#include <string>
+
+
+int id;
+bool canScan = false;
+
 
 //This callback function is responsible for getting the camera-apriltag distance and add a mpde tp tje 
-void transform_callback(const tb_self_experimentation::object_loc &msg)
+void transform_callback(const wasp_custom_msgs::object_loc &msg)
 {
-	static tf::TransformBroadcaster br;
-	tf::Transform transform;
-	transform.setOrigin( tf::Vector3(msg.point.x, msg.point.y, msg.point.z) );
-	tf::Quaternion qt = tf::Quaternion();
-	qt.setRPY(0,-msg.angles.y,0);//pure experimentation with the angles published by the april tag
-	transform.setRotation(qt);
-	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "camera_rgb_frame", "apriltag"));
+	if(canScan)
+	{
+		if(id == msg.ID)
+		{
+			std::cout<<"Receiving the correct id"<<std::endl;
+			static tf::TransformBroadcaster br;
+			tf::Transform transform;
+			//Setting the position of the apriltag related to the drone
+			transform.setOrigin( tf::Vector3(msg.point.x, msg.point.y, msg.point.z) );
+			//We dont care about the rotation of the tag	
+			tf::Quaternion qt = tf::Quaternion();
+			qt.setRPY(0,0,0);//pure experimentation with the angles published by the april tag
+			transform.setRotation(qt);
+
+			br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "ardrone_base_frontcam", "apriltag/"+std::to_string(id)));
+		}
+	}
+}
+
+void scan_callback(const std_msgs::Bool &msg)
+{
+	canScan = msg.data;
 }
 
 int main(int argc, char** argv){
 	ros::init(argc, argv, "apriltag_tf_broadcaster");
 	ros::NodeHandle nh;
-	ros::Subscriber sub =  nh.subscribe("apriltag/distance", 1, &transform_callback);
+	ros::Subscriber sub =  nh.subscribe("object_location", 1, &transform_callback);
+	ros::Subscriber subscan =  nh.subscribe("scan", 1, &scan_callback);
+	if (ros::param::get("~id",id))
+ 	{
+		std::cout<<"Using id  "<<id<<std::endl;
+	}
+	else
+	{
+		id=0;
+		ROS_INFO("No parameter 'id' found. Using id 0 for the april tag");
+	}
+
 	ros::spin();
 };
--- src/drone_tf/src/apriltag_tf_listener.cpp
+++ src/drone_tf/src/apriltag_tf_listener.cpp
@@ -6,26 +6,37 @@
 #include <tf/transform_listener.h>
 #include "geometry_msgs/Pose2D.h"
 
+int id;
 
 int main(int argc, char** argv){
 	ros::init(argc, argv, "apriltag_tf_listener");
 	ros::NodeHandle nh;
-
-	ros::Publisher pub = nh.advertise<geometry_msgs::Pose2D>("apriltag/global_position", 10);
-
 	tf::TransformListener listener;
+	//Getting the private id
+	if (ros::param::get("~id",id))
+ 	{
+		std::cout<<"Using id  "<<id<<std::endl;
+	}
+	else
+	{
+		id=0;
+		ROS_INFO("No parameter 'id' found. Using id 0 for the april tag");
+	}
+
+	//we are publishing to global namespace /apriltag/global_position/id
+	ros::Publisher pub = nh.advertise<geometry_msgs::Pose2D>("/apriltag/global_position/"+std::to_string(id), 10);
+	
 
 	ros::Rate rate(10.0);
 	while (nh.ok()){
 		tf::StampedTransform transform;
 		try{
-			listener.lookupTransform("/map", "/apriltag",ros::Time(0), transform);
+			//We are listening to the topic /droneX/apriltag/id
+			listener.lookupTransform("/map", "apriltag/"+std::to_string(id) ,ros::Time(0), transform);
 			geometry_msgs::Pose2D position;
 			position.x = transform.getOrigin().x();
 			position.y = transform.getOrigin().y();
 			position.theta = tf::getYaw(transform.getRotation());
-			std::cout<< position <<std::endl;
-		
 			pub.publish(position);
 		}
 		catch (tf::TransformException ex){
