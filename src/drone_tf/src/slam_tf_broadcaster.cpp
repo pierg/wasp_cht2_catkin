acbfdefd0ee0ee34aef823e5a6cbb936eebcbb28
David Issa Mattos <issamattos.david@gmail.com>
2016-11-25 14:55:24 +0100

    slam_tf just need to put the correct TF name

--- src/drone_tf/src/slam_tf_broadcaster.cpp
+++ src/drone_tf/src/slam_tf_broadcaster.cpp
@@ -8,51 +8,45 @@
 #include <string>
 
 
-int id;
-bool canScan = false;
+int main(int argc, char** argv){
+	ros::init(argc, argv, "slam_tf_broadcaster");
+	ros::NodeHandle nh;
+	double apriltag_initial_x;
+	double apriltag_initial_y;
 
-
-//This callback function is responsible for getting the camera-apriltag distance and add a mpde tp tje 
-void transform_callback(const wasp_custom_msgs::object_loc &msg)
-{
-	if(canScan)
+	if (ros::param::get("~apriltag_initial_x",apriltag_initial_x))
+ 	{
+		std::cout<<"X initial position  "<<apriltag_initial_x<<std::endl;
+	}
+	else
 	{
-		if(id == msg.ID)
-		{
-			std::cout<<"Receiving the correct id"<<std::endl;
-			static tf::TransformBroadcaster br;
+		apriltag_initial_x=0;
+		ROS_INFO("No parameter 'apriltag_initial_x' found. Using apriltag_initial_x 0 for the apriltag_initial_x");
+	}
+	if (ros::param::get("~apriltag_initial_y",apriltag_initial_y))
+ 	{
+		std::cout<<"Y initial position  "<<apriltag_initial_y<<std::endl;
+	}
+	else
+	{
+		apriltag_initial_y=0;
+		ROS_INFO("No parameter 'apriltag_initial_t' found. Using apriltag_initial_t 0 for the apriltag_initial_x");
+	}
+
+	ros::Rate rate(10.0);
+  	while (nh.ok())
+	{
+		static tf::TransformBroadcaster br;
 			tf::Transform transform;
 			//Setting the position of the apriltag related to the drone
-			transform.setOrigin( tf::Vector3(msg.point.x, msg.point.y, msg.point.z) );
+			transform.setOrigin( tf::Vector3(apriltag_initial_x, apriltag_initial_y, 0));
 			//We dont care about the rotation of the tag	
 			tf::Quaternion qt = tf::Quaternion();
 			qt.setRPY(0,0,0);//pure experimentation with the angles published by the april tag
 			transform.setRotation(qt);
 
-			br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "ardrone_base_frontcam", "apriltag/"+std::to_string(id)));
-		}
+			br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "SLAM_TF/drone_global_position", "/map"));
+	
+		rate.sleep();
 	}
-}
-
-void scan_callback(const std_msgs::Bool &msg)
-{
-	canScan = msg.data;
-}
-
-int main(int argc, char** argv){
-	ros::init(argc, argv, "apriltag_tf_broadcaster");
-	ros::NodeHandle nh;
-	ros::Subscriber sub =  nh.subscribe("object_location", 1, &transform_callback);
-	ros::Subscriber subscan =  nh.subscribe("scan", 1, &scan_callback);
-	if (ros::param::get("~id",id))
- 	{
-		std::cout<<"Using id  "<<id<<std::endl;
-	}
-	else
-	{
-		id=0;
-		ROS_INFO("No parameter 'id' found. Using id 0 for the april tag");
-	}
-
-	ros::spin();
 };
