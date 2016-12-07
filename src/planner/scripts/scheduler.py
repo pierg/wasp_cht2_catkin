19cc5f0a0cd08961d6867ee3dd58044976d89b9b
Piergiuseppe <mallozzi@chalmers.se>
2016-12-07 13:44:30 +0100

    Repushing scheduler.py.... it seems to work here.

--- src/planner/scripts/scheduler.py
+++ src/planner/scripts/scheduler.py
@@ -16,225 +16,225 @@ from std_msgs.msg import String
 from planner.msg import drone_command
 from planningProperties import *
 from planning import updatePlan, printPlans
-from os.path import expanduser 
+from os.path import expanduser
 import generateProblem
 
 def rePlan():
-	SCHEDULER_ACTIVE = False;
-	updatePlan();
-	#for i in ROBOTS:
-		#current[i] = {'index': 1, 'running': False};
-	SCHEDULER_ACTIVE = True;
-	idleMessage(-1);
-	#updateWeb();
+    SCHEDULER_ACTIVE = False;
+    updatePlan();
+    #for i in ROBOTS:
+    #current[i] = {'index': 1, 'running': False};
+    SCHEDULER_ACTIVE = True;
+    idleMessage(-1);
+#updateWeb();
 
 
 def incomingVictim(data):
-	global victims
-	global victim_ids
-	global locations
-	global positions
-	global crates
-	global at
+    global victims
+    global victim_ids
+    global locations
+    global positions
+    global crates
+    global at
 
-	d = data.data.split( )
-	id = victims
-	
-	#check if we have discovered a victim with this ID already, return if so
-	for v in victim_ids:
-		if v == d[2]:
-			return
-		
-	victim_ids.append(d[2]);
+    d = data.data.split( )
+    id = victims
 
-	# Add the new victim
-	victims += 1;
-	name = "victimLocation"+`id`
-	generateProblem.victims += 1; # because I don't have the same instance.....
-	locations.append(name)
-	positions[name] = [int(round(float(d[0]))), int(round(float(d[1])))]
+    #check if we have discovered a victim with this ID already, return if so
+    for v in victim_ids:
+        if v == d[2]:
+            return
 
-	print positions[name]
-	# Add a new crate for the victim
-	crates += 1; 
-	CRATES = range(crates);
-	generateProblem.crates += 1; # because I don't have the same instance.....
-	generateProblem.CRATES = range(crates);
-	at.append("depot")
-	
+    victim_ids.append(d[2]);
 
-	print "New victim identified at positions: ", d[0], ",", d[1]
-	print "Initiate re-planning"
-	rePlan(); # run the planner
+    # Add the new victim
+    victims += 1;
+    name = "victimLocation"+`id`
+    generateProblem.victims += 1; # because I don't have the same instance.....
+    locations.append(name)
+    positions[name] = [int(round(float(d[0]))), int(round(float(d[1])))]
+
+    print positions[name]
+    # Add a new crate for the victim
+    crates += 1;
+    CRATES = range(crates);
+    generateProblem.crates += 1; # because I don't have the same instance.....
+    generateProblem.CRATES = range(crates);
+    at.append("depot")
+
+
+    print "New victim identified at positions: ", d[0], ",", d[1]
+    print "Initiate re-planning"
+    rePlan(); # run the planner
 
 
 
 def incomingCommand(data):
-	global SCHEDULER_ACTIVE
-	global emergency_areas
-	global locations
-	global positions
-	d = data.data.split( )
-	if d[0] == "emergency_area":
-		name = "area"+`emergency_areas`
-		emergency_areas += 1;
-		generateProblem.emergency_areas += 1; # because I don't have the same instance.....
-		locations.append(name)
-		positions[name] = [int(d[1]), int(d[2])]
-		print "Incomming emergency_area"
-		print "Initiate re-planning"
-		rePlan(); # run the planner
-	elif d[0] == "start":
-		SCHEDULER_ACTIVE = True;
-		print "activate the scheduler"
-		idleMessage(-1)
-	elif d[0] == "cancel":
-		SCHEDULER_ACTIVE = False;
-		print "cancel the scheduler"
-	else:
-		print "Command: ", d[0], " has not been implemented."
+    global SCHEDULER_ACTIVE
+    global emergency_areas
+    global locations
+    global positions
+    d = data.data.split( )
+    if d[0] == "emergency_area":
+        name = "area"+`emergency_areas`
+        emergency_areas += 1;
+        generateProblem.emergency_areas += 1; # because I don't have the same instance.....
+        locations.append(name)
+        positions[name] = [int(d[1]), int(d[2])]
+        print "Incomming emergency_area"
+        print "Initiate re-planning"
+        rePlan(); # run the planner
+    elif d[0] == "start":
+        SCHEDULER_ACTIVE = True;
+        print "activate the scheduler"
+        idleMessage(-1)
+    elif d[0] == "cancel":
+        SCHEDULER_ACTIVE = False;
+        print "cancel the scheduler"
+    else:
+        print "Command: ", d[0], " has not been implemented."
 
 def idleMessage(robot):
-	if (robot >= 0 and not available[robot]):
-		finishAction(robot)
-		current[robot]['index'] += 1
-		current[robot]['running'] = False
-		print "##", robot, ": ", current[robot]
-		updateWeb()
-	if SCHEDULER_ACTIVE:
-		for r in ROBOTS:
-			if available[r] and len(plan[r]) > 0 and allowedToStart(r, actions[plan[r][0]][0]):
-				startAction(r)
-				current[r]['running'] = True
-				print "####", r, ": ", current[r]
-				updateWeb()
+    if (robot >= 0 and not available[robot]):
+        finishAction(robot)
+        current[robot]['index'] += 1
+        current[robot]['running'] = False
+        print "##", robot, ": ", current[robot]
+        updateWeb()
+    if SCHEDULER_ACTIVE:
+        for r in ROBOTS:
+            if available[r] and len(plan[r]) > 0 and allowedToStart(r, actions[plan[r][0]][0]):
+                startAction(r)
+                current[r]['running'] = True
+                print "####", r, ": ", current[r]
+                updateWeb()
 
 def startAction(r):
-	p = actions[plan[r][0]][0];
-	if p[0] in ['load', 'unload']:
-		booked[p[2]] = True;
-	print "starting: ", r, p, holds, at;
-	print r, p, available[r];
-	available[r] = False;
-	publish(r, p)
+    p = actions[plan[r][0]][0];
+    if p[0] in ['load', 'unload']:
+        booked[p[2]] = True;
+    print "starting: ", r, p, holds, at;
+    print r, p, available[r];
+    available[r] = False;
+    publish(r, p)
 
 def finishAction(r):
-	if len(plan[r]) > 0:
-		p = actions[plan[r][0]][0];
+    if len(plan[r]) > 0:
+        p = actions[plan[r][0]][0];
 
-		if p[0] == 'pickup':
-			holds[r].append(p[1]); 
-			at[getCI(p[1])] = -1;
-		elif p[0] == 'putdown': 
-			holds[r].remove(p[1]); 
-			at[getCI(p[1])] = at[r];
-		elif p[0] == 'load': 
-			holds[r].remove(p[1]);
-			holds[getTI(p[2])].append(p[1]);
-			booked[p[2]] = False;
-		elif p[0] == 'unload': 
-			holds[r].append(p[1]);
-			holds[getTI(p[2])].remove(p[1]);
-			booked[p[2]] = False;
-		elif p[0] in ['fly', 'drive']:
-			at[r] = p[1];
-		elif p[0] == 'deliver':
-			has[p[3]].append(p[2]);
-			holds[r].remove(p[1]);
-		elif p[0] == 'scan':
-			scanned.append(p[1]);
+        if p[0] == 'pickup':
+            holds[r].append(p[1]);
+            at[getCI(p[1])] = -1;
+        elif p[0] == 'putdown':
+            holds[r].remove(p[1]);
+            at[getCI(p[1])] = at[r];
+        elif p[0] == 'load':
+            holds[r].remove(p[1]);
+            holds[getTI(p[2])].append(p[1]);
+            booked[p[2]] = False;
+        elif p[0] == 'unload':
+            holds[r].append(p[1]);
+            holds[getTI(p[2])].remove(p[1]);
+            booked[p[2]] = False;
+        elif p[0] in ['fly', 'drive']:
+            at[r] = p[1];
+        elif p[0] == 'deliver':
+            has[p[3]].append(p[2]);
+            holds[r].remove(p[1]);
+        elif p[0] == 'scan':
+            scanned.append(p[1]);
 
-		print scanned;
+        print scanned;
 
-		actions[plan[r][0]][1] = True;
-		print "finish: ", r, p, holds, at;
-		plan[r] = plan[r][1:]
-		available[r] = True;
+        actions[plan[r][0]][1] = True;
+        print "finish: ", r, p, holds, at;
+        plan[r] = plan[r][1:]
+        available[r] = True;
 
 def allowedToStart(r, p):
-	if p[0] == 'pickup':
-		return at[getCI(p[1])] == at[r];
-	elif p[0] in ['load', 'unload']: 
-		return at[getTI(p[2])] == at[r] and not booked[p[2]] and (p[1] in holds[getTI(p[2])] if (p[0] == 'unload') else True);
-	elif p[0] == 'drive':
-		return not booked[r-drones] and all(actions[i][1] for i in actions[plan[r][0]][2]);
+    if p[0] == 'pickup':
+        return at[getCI(p[1])] == at[r];
+    elif p[0] in ['load', 'unload']:
+        return at[getTI(p[2])] == at[r] and not booked[p[2]] and (p[1] in holds[getTI(p[2])] if (p[0] == 'unload') else True);
+    elif p[0] == 'drive':
+        return not booked[r-drones] and all(actions[i][1] for i in actions[plan[r][0]][2]);
 
-	return True;
+    return True;
 
 def updateWeb():
-	with open(expanduser("~") + '/wasp_challenge_current_state', 'w') as f:
-		json.dump(current, f)
+    with open(expanduser("~") + '/wasp_challenge_current_state', 'w') as f:
+        json.dump(current, f)
 
 def publish(robot, state):
-	global topic
-	global idByRobot
-	rospy.loginfo("state to be published: "); 
-	rospy.loginfo(state);
-	msg = drone_command();
-	msg.drone_id = idByRobot[robot];
-	#TODO: split case to handle drones as well; currently only turtles
-	msg.command = state[0];
-	if msg.command == 'drive':
-		location = state[1];
-		position = positions[location];
-		msg.posX = position[0];
-		msg.posY = position[1];
-		msg.angle = 0;
-		topic[robot].publish(msg);
-	else:
-		topic[robot].publish(msg);
+    global topic
+    global idByRobot
+    rospy.loginfo("state to be published: ");
+    rospy.loginfo(state);
+    msg = drone_command();
+    msg.drone_id = idByRobot[robot];
+    #TODO: split case to handle drones as well; currently only turtles
+    msg.command = state[0];
+    if msg.command == 'drive':
+        location = state[1];
+        position = positions[location];
+        msg.posX = position[0];
+        msg.posY = position[1];
+        msg.angle = 0;
+        topic[robot].publish(msg);
+    else:
+        topic[robot].publish(msg);
 
 
 def trim(string):
-	if (string[0] == '/'):
-		return string[1:]
-	else:
-		return string
+    if (string[0] == '/'):
+        return string[1:]
+    else:
+        return string
 
 def messageFromUnit(data):
-	if (data.command == 'idle'):
-		unitId = trim(data._connection_header['topic'])
-		unitId2 = data.drone_id
-		if unitId == unitId2:
-			rospy.loginfo('got idle message from %s', unitId)
-			idleMessage(robotById[unitId])
-		else:
-			rospy.loginfo('id/channel mismatch: %s on channel %s', unitId2, unitId)
+    if (data.command == 'idle'):
+        unitId = trim(data._connection_header['topic'])
+        unitId2 = data.drone_id
+        if unitId == unitId2:
+            rospy.loginfo('got idle message from %s', unitId)
+            idleMessage(robotById[unitId])
+        else:
+            rospy.loginfo('id/channel mismatch: %s on channel %s', unitId2, unitId)
 
 
 def communicator():
-	#global rate
-	global topic
-	global robotById
-	global idByRobot
-	topic = {}
-	robotById = {}
-	idByRobot = {}
-	rospy.init_node('scheduler', anonymous=False)
-	#rate = rospy.Rate(2)
-	rospy.loginfo('started scheduler')
+    #global rate
+    global topic
+    global robotById
+    global idByRobot
+    topic = {}
+    robotById = {}
+    idByRobot = {}
+    rospy.init_node('scheduler', anonymous=False)
+    #rate = rospy.Rate(2)
+    rospy.loginfo('started scheduler')
 
-	# subscribe to commands from the operator
-	rospy.Subscriber("wasp_cth_operator", String, incomingCommand)
-	# subscribe to new victims, identified by the drones
-	rospy.Subscriber("wasp_cth_victims", String, incomingVictim)
+    # subscribe to commands from the operator
+    rospy.Subscriber("wasp_cth_operator", String, incomingCommand)
+    # subscribe to new victims, identified by the drones
+    rospy.Subscriber("wasp_cth_victims", String, incomingVictim)
 
 
-	for r in ROBOTS:
-		id = ('drone' if (r < drones) else 'turtle') + str(r)
-		topic[r] = rospy.Publisher(id, drone_command, queue_size=10)
-		rospy.Subscriber(id, drone_command, messageFromUnit)
-		robotById[id] = r;
-		idByRobot[r] = id;
+    for r in ROBOTS:
+        id = ('drone' if (r < drones) else 'turtle') + str(r)
+        topic[r] = rospy.Publisher(id, drone_command, queue_size=10)
+        rospy.Subscriber(id, drone_command, messageFromUnit)
+        robotById[id] = r;
+        idByRobot[r] = id;
 
-	rospy.spin()
+    rospy.spin()
 
 if __name__ == '__main__':
-	try:
-		updateWeb();
-		updatePlan();
-		printPlans();
-		print "###"
-		communicator()
-	except rospy.ROSInterruptException:
-		pass
+    try:
+        updateWeb();
+        updatePlan();
+        printPlans();
+        print "###"
+        communicator()
+    except rospy.ROSInterruptException:
+        pass
--- web.py/view.py
+++ web.py/view.py
@@ -1,15 +1,23 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# WASP Challenge - Chalmers
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Support functions to render a plan
+
 import web
 from generatePlan import readPlan
+import os
 
 cache = False
 
 t_globals = dict(
-  datestr=web.datestr,
+        datestr=web.datestr,
 )
-render = web.template.render('templates/', cache=cache, 
-    globals=t_globals)
+folderpath = os.path.join(os.path.dirname(__file__),'templates/')
+
+render = web.template.render(folderpath, cache=cache, globals=t_globals)
 render._keywords['globals']['render'] = render
 
 
 def list(**k):
-	return render.list(readPlan())
+    return render.list(readPlan())
